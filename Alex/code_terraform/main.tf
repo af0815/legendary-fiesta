5b3c725c2ebd29da0cc7fa29447d7d4dd2ba28db
af0815 <frischmann.alexandra@gmail.com>
2025-03-14 10:44:59 +0000

    VM NIC and Disk created

--- Alex/code_terraform/main.tf
+++ Alex/code_terraform/main.tf
@@ -20,9 +20,48 @@ resource "azurerm_virtual_network" "Alex" {
   address_space       = ["10.0.0.0/16"]
 }
 
+# Create Subnet
 resource "azurerm_subnet" "Alex" {
   name                 = "Alex-subnet"
   resource_group_name  = azurerm_resource_group.Alex.name
   virtual_network_name = azurerm_virtual_network.Alex.name
   address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_network_interface" "Alex-nic" {
+  name                = "Alex-nic"
+  location            = azurerm_resource_group.Alex.location
+  resource_group_name = azurerm_resource_group.Alex.name
+
+  ip_configuration {
+    name                          = "Alex-ipcfg"
+    subnet_id                     = azurerm_subnet.Alex.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+# Define the virtual machine
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "Alex-vm"
+  location            = azurerm_resource_group.Alex.location
+  resource_group_name = azurerm_resource_group.Alex.name
+  network_interface_ids = [
+    azurerm_network_interface.Alex-nic.id,
+  ]
+  size               = "Standard_DS1_v2"
+  admin_username     = "Alex"
+  admin_password     = "34FDA$#214f"  # For demonstration purposes only. Use secure methods for production.
+  disable_password_authentication = "false"
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
 }
