7f4b2540b216c74e1139f99be3bf49ce41863839
af0815 <frischmann.alexandra@gmail.com>
2025-03-14 10:29:25 +0000

    created network + subnet

--- Alex/code_terraform/main.tf
+++ Alex/code_terraform/main.tf
@@ -11,3 +11,18 @@ resource "azurerm_resource_group" "Alex" {
     Owner = "alex.frischmann@redbull.com"
   }
 }
+
+# Create a virtual network within the resource group
+resource "azurerm_virtual_network" "Alex" {
+  name                = "Alex-network"
+  resource_group_name = azurerm_resource_group.Alex.name
+  location            = azurerm_resource_group.Alex.location
+  address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "Alex" {
+  name                 = "Alex-subnet"
+  resource_group_name  = azurerm_resource_group.Alex.name
+  virtual_network_name = azurerm_virtual_network.Alex.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
