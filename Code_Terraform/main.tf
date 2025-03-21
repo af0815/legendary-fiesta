300dda7c8809c2d4c3fdff9f3faacd5efb2e9ab9
af0815 <frischmann.alexandra@gmail.com>
2025-03-21 10:49:42 +0000

    modularized the terraform code

--- Code_Terraform/main.tf
+++ Code_Terraform/main.tf
@@ -1,6 +1,3 @@
-
-#-----Alex-----#
-# Create a resource group
 resource "azurerm_resource_group" "Alex" {
   name     = "Alex-resources"
   location = "West Europe"
@@ -9,156 +6,77 @@ resource "azurerm_resource_group" "Alex" {
     Owner = "alex.frischmann@redbull.com"
   }
 }
- 
- 
-# Create a virtual network within the resource group
-resource "azurerm_virtual_network" "Alex" {
-  name                = "Alex-network"
-  resource_group_name = azurerm_resource_group.Alex.name
-  location            = azurerm_resource_group.Alex.location
-  address_space       = ["10.0.0.0/16"]
-}
- 
-resource "azurerm_subnet" "Alex" {
-  name                 = "Alex-subnet"
-  resource_group_name  = azurerm_resource_group.Alex.name
-  virtual_network_name = azurerm_virtual_network.Alex.name
-  address_prefixes     = ["10.0.1.0/24"]
-}
- 
- 
-# Define the network interface
-resource "azurerm_network_interface" "Alex-nic" {
-  name                = "Alex-nic"
-  location            = azurerm_resource_group.Alex.location
-  resource_group_name = azurerm_resource_group.Alex.name
- 
-  ip_configuration {
-    name                          = "Alex-ipcfg"
-    subnet_id                     = azurerm_subnet.Alex.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.my_terraform_public_ip_Alex.id
-  }
-}
- 
-# Define the virtual machine
-resource "azurerm_linux_virtual_machine" "vm_Alex" {
-  name                = "Alex-vm"
-  location            = azurerm_resource_group.Alex.location
-  resource_group_name = azurerm_resource_group.Alex.name
-  network_interface_ids = [
-    azurerm_network_interface.Alex-nic.id,
-  ]
-  size               = "Standard_B1s"
-  admin_username     = "Alex"
-  admin_password     = "34FDA$#214f"  # For demonstration purposes only. Use secure methods for production.
-  disable_password_authentication = "false"
- 
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
- 
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "18.04-LTS"
-    version   = "latest"
- 
-  }
-}
- 
-# Create public IPs
-resource "azurerm_public_ip" "my_terraform_public_ip_Alex" {
-  name                = "myPublicIP"
-  location            = azurerm_resource_group.Alex.location
-  resource_group_name = azurerm_resource_group.Alex.name
-  allocation_method   = "Dynamic"
-  sku                 = "Basic"
-}
 
-
-#-----Manuel-----#
-# Create a resource group
 resource "azurerm_resource_group" "Manuel" {
   name     = "Manuel-resources"
   location = "West Europe"
-
+ 
   tags = {
     Owner = "manuel.trommer@redbull.com"
   }
 }
 
 
-# Create a virtual network within the resource group
-resource "azurerm_virtual_network" "Manuel" {
-  name                = "Manuel-network"
-  resource_group_name = azurerm_resource_group.Manuel.name
-  location            = azurerm_resource_group.Manuel.location
-  address_space       = ["10.0.0.0/16"]
+module "vnet-Manuel" {
+  source                = "./modules/vnet"
+  vnet_name             = "manuel-vnet"
+  address_space         = ["10.0.0.0/16"]
+  location              = "East US"
+  resource_group_name   = "manuel-rg"
+  subnet_name           = "manuel-subnet"
+  subnet_prefixes       = ["10.0.1.0/24"]
 }
 
-resource "azurerm_subnet" "Manuel" {
-  name                 = "Manuel-subnet"
-  resource_group_name  = azurerm_resource_group.Manuel.name
-  virtual_network_name = azurerm_virtual_network.Manuel.name
-  address_prefixes     = ["10.0.1.0/24"]
+module "vnet-Alex" {
+  source                = "./modules/vnet"
+  vnet_name             = "alex-vnet"
+  address_space         = ["10.1.0.0/16"]
+  location              = "East US"
+  resource_group_name   = "alex-rg"
+  subnet_name           = "alex-subnet"
+  subnet_prefixes       = ["10.1.1.0/24"]
 }
 
-
-# Define the network interface
-
-resource "azurerm_network_interface" "Manuel-nic" {
-  name                = "Manuel-nic"
-  location            = azurerm_resource_group.Manuel.location
-  resource_group_name = azurerm_resource_group.Manuel.name
-
-  ip_configuration {
-    name                          = "Manuel-ipcfg"
-    subnet_id                     = azurerm_subnet.Manuel.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.my_terraform_public_ip_Manuel.id
-  }
+module "vm-Manuel" {
+  source                = "./modules/vm"
+  vm_name               = "manuel-vm"
+  nic_name              = "manuel-nic"
+  location              = azurerm_resource_group.Manuel.location
+  resource_group_name   = azurerm_resource_group.Manuel.name
+  subnet_id             = module.vnet-Manuel.subnet_id
+  vm_size               = "Standard_DS1_v2"
+  os_disk_name          = "manuel-os-disk"
+  image_publisher       = "Canonical"
+  image_offer           = "UbuntuServer"
+  image_sku             = "18.04-LTS"
+  image_version         = "latest"
+  computer_name         = "manuel-computer"
+  admin_username        = "manuel-admin"
+  admin_password        = "P@ssw0rd123!"
+  disable_password_authentication = false
+  public_ip_sku         = "Standard"
+  public_ip_allocation  = "Static"
+  public_ip_name        = "manuel-public-ip"
 }
 
-
-
-# Define the virtual machine
-resource "azurerm_linux_virtual_machine" "vm_Manuel" {
-  name                = "Manuel-vm"
-  location            = azurerm_resource_group.Manuel.location
-  resource_group_name = azurerm_resource_group.Manuel.name
-  network_interface_ids = [
-    azurerm_network_interface.Manuel-nic.id,
-  ]
-  size               = "Standard_B1s"
-  admin_username     = "Manuel"
-  admin_password     = "34FDA$#214f"  # For demonstration purposes only. Use secure methods for production.
-  disable_password_authentication = "false"
-
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
-
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "18.04-LTS"
-    version   = "latest"
-  
-  }
+module "vm-Alex" {
+  source                = "./modules/vm"
+  vm_name               = "alex-vm"
+  nic_name              = "alex-nic"
+  location              = azurerm_resource_group.Alex.location
+  resource_group_name   = azurerm_resource_group.Alex.name
+  subnet_id             = module.vnet-Alex.subnet_id 
+  vm_size               = "Standard_DS1_v2"
+  os_disk_name          = "alex-os-disk"
+  image_publisher       = "Canonical"
+  image_offer           = "UbuntuServer"
+  image_sku             = "18.04-LTS"
+  image_version         = "latest"
+  computer_name         = "alex-computer"
+  admin_username        = "alex-admin"
+  admin_password        = "P@ssw0rd123!"
+  disable_password_authentication = false
+  public_ip_sku         = "Standard"
+  public_ip_allocation  = "Static"
+  public_ip_name        = "alex-public-ip"
 }
-
-# Create public IPs
-resource "azurerm_public_ip" "my_terraform_public_ip_Manuel" {
-  name                = "myPublicIP"
-  location            = azurerm_resource_group.Manuel.location
-  resource_group_name = azurerm_resource_group.Manuel.name
-  allocation_method   = "Dynamic"
-  sku                 = "Basic"
-}
- 
- 
- 
- 
